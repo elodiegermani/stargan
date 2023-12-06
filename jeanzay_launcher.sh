79ad07f9fcb5f64169b8421a6ad66cd6d53cfb8c
elodiegermani1 <elodie.germani@irisa.fr>
2023-12-06 13:50:26 +0100

    4class model

--- jeanzay_launcher.sh
+++ jeanzay_launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#SBATCH --job-name=stargan # nom du job
+#SBATCH --job-name=stargan-4classes # nom du job
 #SBATCH --ntasks=1                   # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
@@ -13,8 +13,14 @@
 
 source /gpfswork/rech/gft/umh25bv/miniconda3/bin/activate /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv
 
+# /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u /gpfswork/rech/gft/umh25bv/stargan/main.py \
+# --mode train --dataset dataset_rh-jeanzay --labels pipelines --image_size 56 \
+# --c_dim 24 --batch_size 16 --data_dir data \
+# --sample_dir samples --log_dir logs \
+# --model_save_dir models --result_dir results 
+
 /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u /gpfswork/rech/gft/umh25bv/stargan/main.py \
---mode train --dataset dataset_rh-jeanzay --labels pipelines --image_size 56 \
---c_dim 24 --batch_size 16 --data_dir data \
---sample_dir samples --log_dir logs \
---model_save_dir models --result_dir results 
+--mode train --dataset dataset_rh_4class-jeanzay --labels pipelines --image_size 56 \
+--c_dim 4 --batch_size 16 --data_dir data \
+--sample_dir samples-4class --log_dir logs \
+--model_save_dir models-4class --result_dir results-4class
