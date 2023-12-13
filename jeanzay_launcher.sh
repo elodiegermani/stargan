21f5d9fa21ea391119a139a3f5693fd4a658623e
elodiegermani1 <elodie.germani@irisa.fr>
2023-12-13 10:38:10 +0100

    24 classes

--- jeanzay_launcher.sh
+++ jeanzay_launcher.sh
@@ -6,7 +6,7 @@
 #SBATCH --cpus-per-task=16           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --distribution=block:block   # we pin the tasks on contiguous cores
-#SBATCH --time=48:00:00              # maximum execution time (HH:MM:SS)
+#SBATCH --time=90:00:00              # maximum execution time (HH:MM:SS)
 #SBATCH --output=stargan%j.out # output file name
 #SBATCH --error=stargan%j.err  # error file name
 #SBATCH --qos=qos_gpu-t4
@@ -14,15 +14,15 @@
 
 source /gpfswork/rech/gft/umh25bv/miniconda3/bin/activate /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv
 
-/gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u main.py --mode test --dataset dataset_rh_4class-jeanzay --labels pipelines --image_size 56 \
---c_dim 4 --batch_size 1 --data_dir data \
---sample_dir samples-4class --log_dir logs \
---model_save_dir models-4class --result_dir results --test_iters 50000
+# /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u main.py --mode test --dataset dataset_rh_4class-jeanzay --labels pipelines --image_size 56 \
+# --c_dim 4 --batch_size 1 --data_dir data \
+# --sample_dir samples-4class --log_dir logs \
+# --model_save_dir models-4class --result_dir results --test_iters 50000
 
-# /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u main.py --mode test --dataset dataset_rh-jeanzay --labels pipelines --image_size 56 \
-# --c_dim 24 --batch_size 1 --data_dir data \
-# --sample_dir samples --log_dir logs \
-# --model_save_dir models --result_dir results --test_iters 200000
+/gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u main.py --mode test --dataset dataset_rh-jeanzay --labels pipelines --image_size 56 \
+--c_dim 24 --batch_size 1 --data_dir data \
+--sample_dir samples --log_dir logs \
+--model_save_dir models --result_dir results --test_iters 200000
 
 # /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u /gpfswork/rech/gft/umh25bv/stargan/main.py \
 # --mode train --dataset dataset_rh-jeanzay --labels pipelines --image_size 56 \
