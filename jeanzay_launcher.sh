014d2f5417eda767688b39432eaf51233b8dc9e9
elodiegermani1 <elodie.germani@irisa.fr>
2024-01-09 13:59:36 +0100

    Launcher 2 classes 30grp

--- jeanzay_launcher.sh
+++ jeanzay_launcher.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=stargan-4class-30grp # nom du job
+#SBATCH --job-name=stargan-2class-30grp # nom du job
 #SBATCH --ntasks=1                   # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
 #SBATCH --cpus-per-task=16           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --distribution=block:block   # we pin the tasks on contiguous cores
-#SBATCH --time=32:00:00              # maximum execution time (HH:MM:SS)
-#SBATCH --output=stargan-4class-30grp%j.out # output file name
-#SBATCH --error=stargan-4class-30grp%j.err  # error file name
+#SBATCH --time=12:00:00              # maximum execution time (HH:MM:SS)
+#SBATCH --output=stargan-2class-30grp%j.out # output file name
+#SBATCH --error=stargan-2class-30grp%j.err  # error file name
 #SBATCH --qos=qos_gpu-t4
 
 source /gpfswork/rech/gft/umh25bv/miniconda3/bin/activate /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv
@@ -38,7 +38,7 @@ source /gpfswork/rech/gft/umh25bv/miniconda3/bin/activate /gpfswork/rech/gft/umh
 # --model_save_dir models-4class --result_dir results-4class
 
 /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u /gpfswork/rech/gft/umh25bv/stargan/main.py \
---mode train --dataset dataset_rh_4class_30grp-jeanzay --labels pipelines --image_size 56 \
---c_dim 4 --batch_size 16 --data_dir data \
---sample_dir samples-4class-30grp --log_dir logs \
---model_save_dir models-4class-30grp --result_dir results-4class-30grp
+--mode train --dataset dataset_rh_2class_30grp-jeanzay --labels pipelines --image_size 56 \
+--c_dim 2 --batch_size 16 --data_dir data \
+--sample_dir samples-2class-30grp --log_dir logs \
+--model_save_dir models-2class-30grp --result_dir results-2class-30grp
