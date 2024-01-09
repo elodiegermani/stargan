d7ab71467c752695379fdb513296a2f272610502
elodiegermani1 <elodie.germani@irisa.fr>
2024-01-09 13:35:30 +0100

    Training on 2 classes

--- jeanzay_launcher.sh
+++ jeanzay_launcher.sh
@@ -1,23 +1,24 @@
 #!/bin/bash
-#SBATCH --job-name=preprocessing # nom du job
+#SBATCH --job-name=stargan-2class # nom du job
 #SBATCH --ntasks=1                   # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
 #SBATCH --cpus-per-task=16           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --distribution=block:block   # we pin the tasks on contiguous cores
-#SBATCH --time=12:00:00              # maximum execution time (HH:MM:SS)
-#SBATCH --output=preprocess%j.out # output file name
-#SBATCH --error=preprocess%j.err  # error file name
+#SBATCH --time=32:00:00              # maximum execution time (HH:MM:SS)
+#SBATCH --output=stargan-2class%j.out # output file name
+#SBATCH --error=stargan-2class%j.err  # error file name
+#SBATCH --qos=qos_gpu-t4
 
 source /gpfswork/rech/gft/umh25bv/miniconda3/bin/activate /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv
 
 # /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u preprocessing.py
 
-/gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u main.py --mode test --dataset dataset_rf_4class-jeanzay --labels pipelines --image_size 56 \
---c_dim 4 --batch_size 1 --data_dir data \
---sample_dir samples-4class-rf --log_dir logs \
---model_save_dir models-4class --result_dir results --test_iters 50000
+# /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u main.py --mode test --dataset dataset_rf_4class-jeanzay --labels pipelines --image_size 56 \
+# --c_dim 4 --batch_size 1 --data_dir data \
+# --sample_dir samples-4class-rf --log_dir logs \
+# --model_save_dir models-4class --result_dir results --test_iters 50000
 
 # /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u main.py --mode test --dataset dataset_rh-jeanzay --labels pipelines --image_size 56 \
 # --c_dim 24 --batch_size 1 --data_dir data \
@@ -35,3 +36,9 @@ source /gpfswork/rech/gft/umh25bv/miniconda3/bin/activate /gpfswork/rech/gft/umh
 # --c_dim 4 --batch_size 16 --data_dir data \
 # --sample_dir samples-4class --log_dir logs \
 # --model_save_dir models-4class --result_dir results-4class
+
+/gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u /gpfswork/rech/gft/umh25bv/stargan/main.py \
+--mode train --dataset dataset_rh_2class-jeanzay --labels pipelines --image_size 56 \
+--c_dim 2 --batch_size 16 --data_dir data \
+--sample_dir samples-2class --log_dir logs \
+--model_save_dir models-2class --result_dir results-2class
