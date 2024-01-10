0701c88533e7617d9c26f169b98380a42fca4d81
elodiegermani1 <elodie.germani@irisa.fr>
2024-01-10 11:24:57 +0100

    Reorganize

--- src/jeanzay_launcher.sh
+++ src/jeanzay_launcher.sh
@@ -20,7 +20,7 @@ source /gpfswork/rech/gft/umh25bv/miniconda3/bin/activate /gpfswork/rech/gft/umh
 # --sample_dir samples-2class-30grp --log_dir logs \
 # --model_save_dir models-2class-30grp --result_dir results --test_iters 50000
 
-/gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u main.py --mode test --dataset dataset_rh_4class-jeanzay --labels pipelines --image_size 56 \
+/gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u /gpfswork/rech/gft/umh25bv/stargan/src/main.py --mode test --dataset dataset_rh_4class-jeanzay --labels pipelines --image_size 56 \
 --c_dim 4 --batch_size 1 --data_dir ../data \
 --sample_dir ../samples-4class-30grp \
 --model_save_dir ../models-4class-30grp --test_iters 50000
