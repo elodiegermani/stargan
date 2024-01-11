3367dee2350559b60f6f59de931eef85307c689c
elodiegermani1 <elodie.germani@irisa.fr>
2024-01-11 15:32:25 +0100

    Test 30 groupes spm

--- src/jeanzay_launcher.sh
+++ src/jeanzay_launcher.sh
@@ -15,40 +15,8 @@ source /gpfswork/rech/gft/umh25bv/miniconda3/bin/activate /gpfswork/rech/gft/umh
 
 # /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u preprocessing.py
 
-# /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u main.py --mode test --dataset dataset_rh_2class-jeanzay --labels pipelines --image_size 56 \
-# --c_dim 2 --batch_size 1 --data_dir data \
-# --sample_dir samples-2class-30grp --log_dir logs \
-# --model_save_dir models-2class-30grp --result_dir results --test_iters 50000
-
-# /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u /gpfswork/rech/gft/umh25bv/stargan/src/main.py --mode test --dataset dataset_rh_2class-jeanzay --labels pipelines --image_size 56 \
-# --c_dim 2 --batch_size 1 --data_dir data \
-# --sample_dir samples-2class \
-# --model_save_dir models-2class --test_iters 50000
-
-# /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u main.py --mode test --dataset dataset_rh_2class-jeanzay --labels pipelines --image_size 56 \
-# --c_dim 2 --batch_size 1 --data_dir data \
-# --sample_dir samples-2class --log_dir logs \
-# --model_save_dir models-2class --result_dir results --test_iters 50000
-
-# /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u main.py --mode test --dataset dataset_rh-jeanzay --labels pipelines --image_size 56 \
-# --c_dim 24 --batch_size 1 --data_dir data \
-# --sample_dir samples --log_dir logs \
-# --model_save_dir models --result_dir results --test_iters 200000
-
-# /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u /gpfswork/rech/gft/umh25bv/stargan/main.py \
-# --mode train --dataset dataset_rh-jeanzay --labels pipelines --image_size 56 \
-# --c_dim 24 --batch_size 16 --data_dir data \
-# --sample_dir samples --log_dir logs \
-# --model_save_dir models --result_dir results 
-
-# /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u /gpfswork/rech/gft/umh25bv/stargan/main.py \
-# --mode train --dataset dataset_rh_4class-jeanzay --labels pipelines --image_size 56 \
-# --c_dim 4 --batch_size 16 --data_dir data \
-# --sample_dir samples-4class --log_dir logs \
-# --model_save_dir models-4class --result_dir results-4class
-
 /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u /gpfswork/rech/gft/umh25bv/stargan/src/main.py \
---mode train --dataset dataset_rh_2class_30grp_spm-jeanzay --labels pipelines --image_size 56 \
---c_dim 2 --batch_size 16 --data_dir data \
+--mode test --dataset dataset_rh_2class_spm-jeanzay --labels pipelines --image_size 56 \
+--c_dim 2 --batch_size 1 --data_dir data \
 --sample_dir samples-2class-30grp-spm --log_dir logs \
---model_save_dir models-2class-30grp-spm --result_dir results-2class-30grp-spm
+--model_save_dir models-2class-30grp-spm --test-iter 50000 
