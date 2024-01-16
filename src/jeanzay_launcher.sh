cf3c10e43a740f2e04ae9a70049c5c05842109f7
elodiegermani1 <elodie.germani@irisa.fr>
2024-01-16 15:19:19 +0100

    Train 4class

--- src/jeanzay_launcher.sh
+++ src/jeanzay_launcher.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
-#SBATCH --job-name=test-stargan-2class # nom du job
+#SBATCH --job-name=train-stargan-4class # nom du job
 #SBATCH --ntasks=1                   # number of MP tasks
 #SBATCH --ntasks-per-node=1          # number of MPI tasks per node
 #SBATCH --gres=gpu:1                 # number of GPUs per node
 #SBATCH --cpus-per-task=16           # number of cores per tasks
 #SBATCH --hint=nomultithread         # we get physical cores not logical
 #SBATCH --distribution=block:block   # we pin the tasks on contiguous cores
-#SBATCH --time=12:00:00              # maximum execution time (HH:MM:SS)
-#SBATCH --output=stargan-2class-30grp%j.out # output file name
-#SBATCH --error=stargan-2class-30grp%j.err  # error file name
+#SBATCH --time=99:00:00              # maximum execution time (HH:MM:SS)
+#SBATCH --output=stargan-4class-train%j.out # output file name
+#SBATCH --error=stargan-4class-train%j.err  # error file name
 #SBATCH --qos=qos_gpu-t4
 
 source /gpfswork/rech/gft/umh25bv/miniconda3/bin/activate /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv
@@ -16,7 +16,8 @@ source /gpfswork/rech/gft/umh25bv/miniconda3/bin/activate /gpfswork/rech/gft/umh
 # /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u preprocessing.py
 
 /gpfswork/rech/gft/umh25bv/miniconda3/envs/workEnv/bin/python3 -u /gpfswork/rech/gft/umh25bv/stargan/src/main.py \
---mode test --dataset dataset_rh_4class-jeanzay --labels pipelines --image_size 56 \
+--mode train --dataset dataset_rh_4class-jeanzay --labels pipelines --image_size 56 \
 --c_dim 4 --batch_size 1 --data_dir data \
---sample_dir samples-4class-60000iters --log_dir logs \
---model_save_dir models-4class --test_iters 60000 
+--sample_dir samples-4class --log_dir logs \
+--model_save_dir models-4class 
+#--test_iters 60000 
